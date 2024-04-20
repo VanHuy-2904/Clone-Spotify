017bfb522b4c24cf4dd81c4b79158cac5e9314e4
VanHuy <huyle468a@gmail.com>
2024-04-20 14:45:59 +0700

    [fix] đổi cách viết subscribe

--- src/app/Components/callback/callback.component.ts
+++ src/app/Components/callback/callback.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../Service/auth/auth.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-callback',
   standalone: true,
   imports: [],
   templateUrl: './callback.component.html',
-  styleUrl: './callback.component.scss'
+  styleUrl: './callback.component.scss',
 })
 export class CallbackComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
   ) {}
   ngOnInit(): void {
     this.route.queryParams.subscribe((codes: any) => {
@@ -24,22 +25,19 @@ export class CallbackComponent implements OnInit {
     });
   }
   handleCodeExchange(code: string): void {
-    this.authService.exchangeCode(code).subscribe(
-      (data) => {
-        console.log('exchange code: ', data);
+    this.authService.exchangeCode(code).subscribe({
+      next: (data: any) => {
         this.authService.setToken(data.access_token);
-        console.log(localStorage.getItem('token'));
-        this.authService.getUserinfo(data.access_token).subscribe((dataUser) => {
-          console.log(dataUser);
-          
-          localStorage.setItem('nameUser', dataUser.display_name);
-          console.log(localStorage.getItem('nameUser'));
-          this.router.navigate(['/']);
-        });
+        this.authService
+          .getUserinfo(data.access_token)
+          .subscribe((dataUser) => {
+            localStorage.setItem('nameUser', dataUser.display_name);
+            this.router.navigate(['/']);
+          });
       },
-      (error) => {
-        console.log(error);
-      },  
-    );
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
   }
 }
