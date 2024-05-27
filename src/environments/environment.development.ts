5076291fd735e8139ebca20ffb8b80ca50ec25dc
VanHuy <huyle468a@gmail.com>
2024-05-27 18:32:05 +0700

    [feature] deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   clientId: '87ac444283e74b2caa2a9be4134a8d67',
   clientSecret: '0b8b21dbb9d84255ad0c94681ae3d56e',
-  redirectUri: 'https://clone-spotify-iszk.vercel.app/callback',
+  redirectUri:
+    'clone-spotify-iszk-e9df7mnrp-van-huys-projects.vercel.app/callback',
   apiConfig: 'https://api.spotify.com/v1',
   scope:
     'ugc-image-upload streaming user-read-private user-read-email user-modify-playback-state user-read-currently-playing user-library-read playlist-modify-public playlist-modify-private user-top-read user-read-playback-state',
