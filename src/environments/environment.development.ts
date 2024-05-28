08d4bd29a2a38642298879425ed34f0135b09afd
VanHuy <huyle468a@gmail.com>
2024-05-28 15:06:58 +0700

    [fix] fix enviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,8 @@
 export const environment = {
-  production: true,
+  production: false,
   clientId: '87ac444283e74b2caa2a9be4134a8d67',
   clientSecret: '0b8b21dbb9d84255ad0c94681ae3d56e',
-  redirectUri:
-    'http://localhost:4200/callback' ||
-    'https://clone-spotify-iszk.vercel.app/callback',
+  redirectUri: 'http://localhost:4200/callback',
   apiConfig: 'https://api.spotify.com/v1',
   scope:
     'ugc-image-upload streaming user-read-private user-read-email user-modify-playback-state user-read-currently-playing user-library-read playlist-modify-public playlist-modify-private user-top-read user-read-playback-state',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: false,
+  production: true,
+  redirectUri: 'https://clone-spotify-iszk.vercel.app/callback',
 };
